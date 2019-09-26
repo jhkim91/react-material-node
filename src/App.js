c90bdea1a522a9df9b629bfffee2b18c6bedcd03
junhee <developer9101@gmail.com>
2019-09-26 10:23:45 +0900

    Create Customer.js 컴포넌트 구조화

--- src/App.js
+++ src/App.js
@@ -2,21 +2,46 @@ import React from 'react';
 import Customer from "./components/Customer";
 import './App.css';
 
-const customer = {
+const customers = [{
+    'id': 1,
+    'images': 'http://placeimg.com/64/64/any',
     'name': '홍길동',
     'birthday': '900909',
     'gender': '남자',
-    'jab': '학생'
-};
+    'job': '학생'
+},{
+    'id': 2,
+    'images': 'http://placeimg.com/64/64/1',
+    'name': '홍길동2',
+    'birthday': '910909',
+    'gender': '남자2',
+    'job': '학생2'
+},{
+    'id': 3,
+    'images': 'http://placeimg.com/64/64/2',
+    'name': '홍길동3',
+    'birthday': '920909',
+    'gender': '남자3',
+    'job': '학생3'
+}];
 
 function App() {
   return (
-    <Customer
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        jab={customer.jab}
-    />
+      <>
+          {customers.map(c => {
+              return (
+                  <Customer
+                      key={c.id}
+                      id={c.id}
+                      images={c.images}
+                      name={c.name}
+                      birthday={c.birthday}
+                      gender={c.gender}
+                      job={c.job}
+                  />
+              )
+          })}
+      </>
   );
 }
 
